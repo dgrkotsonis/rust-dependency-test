3aad4a5e728ae3477af721d3fc4d22451e6ce465
Gabe K <dgrkotsonis@gmail.com>
2025-02-17 21:27:20 -0500

    Testing dependency

---
+++ test_rust_app/src/main.rs
@@ -0,0 +1,10 @@
+use basic_math_lib::{add, multiply};
+
+fn main() {
+    println!("Hello, world!");
+
+    let sum = add(2, 3);
+    let product = multiply(4, 5);
+    println!("Sum: {}", sum);
+    println!("Product: {}", product);
+}
